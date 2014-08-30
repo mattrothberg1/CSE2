2f3d3c32812ce1bfd9dbc5cea88eb07e74d1ed36
mattrothberg1 <mdr218@lehigh.edu>
2014-08-30 21:11:41 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//Matt Rothberg
+//Demo 0
+//WelcomeClass Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//   add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" /\\ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-M--D--R--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My name is Matt and I'm from");
+    System.out.println(" Port Washington in Long Island");
+    
+    //test/testtest
+  }
+}
